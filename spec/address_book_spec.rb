ae98c377c182c64d646ee2400ea865a0c551c54b
Emily Paige Brown <brown.emily.paige@gmail.com>
2017-12-01 21:45:15 +0000

    assignment 4

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -17,7 +17,22 @@
        expect(book.entries.size).to eq(0)
      end
    end
- 
+   
+   describe "remove_entry"
+    it "removes an entry using name, phone_number, and email" do
+        book = AddressBook.new
+        book.add_entry('Wallace King', '010.012.4576', 'wallyking@lovelace.com')
+        name= "Ada Lovelace"
+        phone_number = '010.012.1815'
+        email = 'augusta.king@lovelace.com' 
+        book.add_entry(name, phone_number, email)
+        
+        expect(book.entries.size).to eq 2
+        book.remove_entry(name, phone_number, email)
+         expect(book.entries.size).to eq 1
+         expect(book.entries.first.name).to eq('Wallace King')
+        end
+    end
   describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
@@ -36,4 +51,3 @@
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
- end
